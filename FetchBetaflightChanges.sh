8d51be674aaa0cbbf2cce07cf80a776089dd4199
Michael Sabielny <michael.sabielny@gmx.de>
2019-11-16 16:55:45 +0100

    Addded some links to docs on github.com and changed the name of the remote from betaflight to upstream

--- FetchBetaflightChanges.sh
+++ FetchBetaflightChanges.sh
@@ -2,10 +2,15 @@
 
 # HELP: https://github.com/betaflight/betaflight/blob/master/docs/development/Development.md
 # Next line only once necessary
-# git remote add betaflight https://github.com/betaflight/betaflight-configurator.git
+# git remote add upstream https://github.com/betaflight/betaflight-configurator.git
 
+
+# see also:
+# https://help.github.com/en/github/getting-started-with-github/fork-a-repo
+# https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/syncing-a-fork
+
+git fetch upstream
 git checkout master
-git fetch betaflight
-git merge betaflight/master
+git merge upstream/master
 git push origin master
 
